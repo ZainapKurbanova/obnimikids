cf7bb48fc399ac18ed4f0112f05b8d03887c0967
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-06-03 13:16:37 +0300

    Delete main directory

--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index_view(request):
-    return render(request, 'main/main.html')
-
-def about_view(request):
-    return render(request, 'main/about.html')
